1909279d350715b8908c525c8258a259d73b3fe4
katrepalin <trepalin.konstantin@gmail.com>
2022-11-20 22:36:48 +0400

    Order, OrderItem models added

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, OrderItem, Order
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['item']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
 
 
 class ItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'price', 'description']
+    list_filter = ['name', 'price']
+    list_editable = ['price', 'description']
 
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Item, ItemAdmin)
--- products/models.py
+++ products/models.py
@@ -6,8 +6,34 @@ class Item(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField(default=0)  # Hundredths of currency
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Item'
+        verbose_name_plural = 'Items'
 
     def get_display_price(self):
         return "{0:.2f}".format(self.price / 100)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item)
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items')
+    item = models.ForeignKey(Item, related_name='order_items')
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
